2bc31efea35f68b03af11aed738bd609ee0c3aa8
Djamel Feddad <de.feddad@gmail.com>
2014-05-06 08:36:45 +0800

    added Response helper class

---
+++ app/src/Paxifi/Support/Response/Response.php
@@ -0,0 +1,286 @@
+<?php namespace Paxifi\Support\Response;
+
+use League\Fractal\Manager;
+
+/**
+ * The Response class helper
+ * @package Paxifi\Support\Response
+ */
+class Response
+{
+    /**
+     * @var int
+     */
+    protected $statusCode;
+
+    /**
+     * @var array
+     */
+    protected $headers;
+
+    /**
+     * @var \League\Fractal\TransformerAbstract
+     */
+    protected $transformer;
+
+    /**
+     * @var string|array|object
+     */
+    protected $content;
+
+    /**
+     * @var \Paxifi\Support\Response\Paginator
+     */
+    protected $paginator;
+
+    /**
+     * @var \Paxifi\Support\Response\Cursor
+     */
+    protected $cursor;
+
+    /**
+     * @var \Paxifi\Support\Response\Item
+     */
+    protected $item;
+
+    /**
+     * @var \Paxifi\Support\Response\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var \League\Fractal\Manager;
+     */
+    protected $fractal;
+
+    /**
+     * Create the Response instance.
+     *
+     * @param Manager $fractal
+     * @param Item $item
+     * @param Collection $collection
+     * @param Paginator $paginator
+     * @param Cursor $cursor
+     */
+    function __construct(Manager $fractal, Item $item, Collection $collection, Paginator $paginator, Cursor $cursor)
+    {
+        $this->fractal = $fractal;
+        $this->item = $item;
+        $this->collection = $collection;
+        $this->paginator = $paginator;
+        $this->cursor = $cursor;
+    }
+
+    /**
+     * Sets the collection resource.
+     *
+     * @param \Paxifi\Support\Response\Collection $collection
+     */
+    public function setCollection($collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * Retrieves the collection resource.
+     *
+     * @return \Paxifi\Support\Response\Collection
+     */
+    public function getCollection()
+    {
+        return $this->collection;
+    }
+
+    /**
+     * Sets the response content.
+     *
+     * @param array|object|string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Retrieves the response content.
+     *
+     * @return array|object|string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Sets the cursor instance.
+     *
+     * @param \Paxifi\Support\Response\Cursor $cursor
+     */
+    public function setCursor($cursor)
+    {
+        $this->cursor = $cursor;
+    }
+
+    /**
+     * Retrieves the cursor instance.
+     *
+     * @return \Paxifi\Support\Response\Cursor
+     */
+    public function getCursor()
+    {
+        return $this->cursor;
+    }
+
+    /**
+     * Sets the response headers.
+     *
+     * @param array $headers
+     */
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * Retrieves the response headers.
+     *
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Sets the item resource.
+     *
+     * @param \Paxifi\Support\Response\Item $item
+     */
+    public function setItem($item)
+    {
+        $this->item = $item;
+    }
+
+    /**
+     * Retrieves the item resource.
+     *
+     * @return \Paxifi\Support\Response\Item
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * Sets the paginator instance.
+     *
+     * @param \Paxifi\Support\Response\Paginator $paginator
+     */
+    public function setPaginator($paginator)
+    {
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * Retrieves the paginator instance.
+     *
+     * @return \Paxifi\Support\Response\Paginator
+     */
+    public function getPaginator()
+    {
+        return $this->paginator;
+    }
+
+    /**
+     * Sets the response status code.
+     *
+     * @param int $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * Retrieves the response status code.
+     *
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Sets the transformer instance.
+     *
+     * @param \League\Fractal\TransformerAbstract $transformer
+     */
+    public function setTransformer($transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * Retrieves the transformer instance.
+     *
+     * @return \League\Fractal\TransformerAbstract
+     */
+    public function getTransformer()
+    {
+        return $this->transformer;
+    }
+
+    /**
+     * Responds with single resource.
+     *
+     * @return array
+     */
+    public function withItem()
+    {
+        $this->item->setData($this->content);
+        $this->item->setTransformer($this->transformer);
+
+        return $this->fractal->createData($this->item)->toArray();
+    }
+
+    /**
+     * Responds with collection.
+     *
+     * @param bool $paginationEnabled
+     *
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    public function withCollection($paginationEnabled = false)
+    {
+        $this->collection->setData($this->content);
+        $this->collection->setTransformer($this->transformer);
+
+        $this->paginate($paginationEnabled);
+
+        return $this->fractal->createData($this->collection)->toArray();
+    }
+
+    /**
+     * Paginates the collection response.
+     *
+     * @param $paginationEnabled
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function paginate($paginationEnabled)
+    {
+        if ($paginationEnabled) {
+
+            if (!$this->content instanceof Paginator) {
+                throw new \InvalidArgumentException();
+            }
+
+            $this->paginator->setupPaginator($this->content);
+
+            $this->collection->setPaginator($this->paginator);
+        }
+    }
+
+} 
---
+++ app/tests/Paxifi/Test/Support/Response/ResponseTest.php
@@ -0,0 +1,99 @@
+<?php namespace Paxifi\Test\Support\Response;
+
+use Mockery as m;
+use Paxifi\Support\Response\Response;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRespondWithItem()
+    {
+        $response = new Response(
+            $fractal = m::mock('League\Fractal\Manager'),
+            $item = m::mock('Paxifi\Support\Response\Item'),
+            m::mock('Paxifi\Support\Response\Collection'),
+            m::mock('Paxifi\Support\Response\Paginator'),
+            m::mock('Paxifi\Support\Response\Cursor')
+        );
+
+        $item->shouldReceive('setData')->andReturn('foo');
+        $item->shouldReceive('setTransformer');
+
+        $fractal->shouldReceive('createData')->with($item)->andReturn('bar');
+
+        $response->withItem();
+    }
+
+    public function testRespondWithCollection()
+    {
+        $response = new Response(
+            $fractal = m::mock('League\Fractal\Manager'),
+            m::mock('Paxifi\Support\Response\Item'),
+            $collection = m::mock('Paxifi\Support\Response\Collection'),
+            m::mock('Paxifi\Support\Response\Paginator'),
+            m::mock('Paxifi\Support\Response\Cursor')
+        );
+
+        $response->setContent('foo');
+        $response->setTransformer($transformer = m::mock('League\Fractal\TransformerAbstract'));
+
+        $fractal->shouldReceive('createData')->once()->with($collection)->andReturn('bar');
+
+        $collection->shouldReceive('setData')->once()->with('foo');
+        $collection->shouldReceive('setTransformer')->once()->with($transformer);
+
+        $response->withCollection();
+    }
+
+    public function testRespondWithPaginatedCollection()
+    {
+        $response = new Response(
+            $fractal = m::mock('League\Fractal\Manager'),
+            m::mock('Paxifi\Support\Response\Item'),
+            $collection = m::mock('Paxifi\Support\Response\Collection'),
+            $paginator = m::mock('Paxifi\Support\Response\Paginator'),
+            m::mock('Paxifi\Support\Response\Cursor')
+        );
+
+        $response->setContent($paginatedContent = m::mock('Paxifi\Support\Response\Paginator'));
+        $response->setTransformer($transformer = m::mock('League\Fractal\TransformerAbstract'));
+
+        $fractal->shouldReceive('createData')->once()->with($collection)->andReturn('bar');
+
+        $collection->shouldReceive('setData')->once()->with($paginatedContent)->andReturn('foo');
+        $collection->shouldReceive('setTransformer')->once()->with($transformer);
+        $collection->shouldReceive('setPaginator')->once()->with($paginator);
+
+        $paginator->shouldReceive('setupPaginator')->once()->with($paginatedContent);
+
+        $response->withCollection(true);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testThrowExceptionIfContentIsNotPaginatorAndRespondWithPaginatedCollection()
+    {
+        $response = new Response(
+            $fractal = m::mock('League\Fractal\Manager'),
+            m::mock('Paxifi\Support\Response\Item'),
+            $collection = m::mock('Paxifi\Support\Response\Collection'),
+            $paginator = m::mock('Paxifi\Support\Response\Paginator'),
+            m::mock('Paxifi\Support\Response\Cursor')
+        );
+
+        $response->setContent('foo');
+        $response->setTransformer($transformer = m::mock('League\Fractal\TransformerAbstract'));
+
+        $collection->shouldReceive('setData')->once()->with('foo');
+        $collection->shouldReceive('setTransformer')->once()->with($transformer);
+
+        $response->withCollection(true);
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+}
+ 
