80d800da30e0863b57d20e678dbdea27e86f78e2
Djamel Feddad <djamel@mobilenowgroup.com>
2014-05-09 14:15:14 +0800

    added create drivers table migration

---
+++ app/database/migrations/2014_05_08_084507_create_drivers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar_url');
+            $table->text('address');
+            $table->string('currency', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('drivers');
+    }
+
+}
