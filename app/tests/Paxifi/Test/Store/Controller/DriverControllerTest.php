6989ee9af3d3292fa2f7607b7d183abe38002c07
Djamel Feddad <de.feddad@gmail.com>
2014-05-12 02:32:03 +0800

    added test for creating/registering new driver

--- app/tests/Paxifi/Test/Store/Controller/DriverControllerTest.php
+++ app/tests/Paxifi/Test/Store/Controller/DriverControllerTest.php
@@ -1,29 +1,49 @@
 <?php namespace Paxifi\Test\Store\Controller;
 
 use Mockery as m;
-use Paxifi\Store\Controller\DriverController;
+use Faker\Factory as Faker;
 
 class DriverControllerTest extends \TestCase
 {
-    public function testImplementsGetTransformer()
+    public function setUp()
     {
-//        $c = new DriverController(m::mock('Paxifi\Store\Repository\DriverRepositoryInterface'), m::mock('Paxifi\Store\Transformer\DriverTransformer'));
-//
-//        $this->assertInstanceOf('\Paxifi\Store\Transformer\DriverTransformer', $c->getTransformer());
+        parent::setUp();
+
+        $this->app['artisan']->call('migrate');
     }
 
     public function testRegisterNewDriver()
     {
-        $postData = ['foo', 'bar'];
+        $faker = Faker::create();
+
+        $email = $faker->email;
+
+        $postData = array(
+            'email' => $email,
+            'password' => \Hash::make($faker->name),
+            'photo' => $faker->imageUrl(250, 250),
+            'name' => $faker->name,
+            'address' => array(
+                'street' => $faker->streetAddress,
+                'city' => $faker->city,
+                'country' => $faker->country,
+                'postcode' => $faker->postcode
+            ),
+            'currency' => 'USD',
+        );
 
         $response = $this->call('post', 'drivers', $postData);
 
         $this->assertResponseStatus(201);
+
+        $this->assertContains($email, $response->getContent());
     }
 
     public function tearDown()
     {
         m::close();
+
+        $this->app['artisan']->call('migrate:reset');
     }
 }
  
