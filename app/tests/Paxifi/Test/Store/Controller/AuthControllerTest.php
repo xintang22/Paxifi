80735c99d63b08701998a8a0ebce453b34873efb
Djamel Feddad <de.feddad@gmail.com>
2014-05-13 08:56:38 +0800

    added test for AuthController

---
+++ app/tests/Paxifi/Test/Store/Controller/AuthControllerTest.php
@@ -0,0 +1,20 @@
+<?php namespace Paxifi\Test\Store\Controller;
+
+class AuthControllerTest extends \TestCase
+{
+    public function testLoginWithValidCredentials()
+    {
+        // TODO: Write test
+    }
+
+    public function testLoginWithWrongCredentials()
+    {
+        // TODO: Write test
+    }
+
+    public function testLogout()
+    {
+        // TODO: Write test
+    }
+}
+ 
