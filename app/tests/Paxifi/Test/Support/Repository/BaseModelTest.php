131899d22b6d7758af0928bab8e386c6a82d55ab
Djamel Feddad <de.feddad@gmail.com>
2014-05-13 08:52:41 +0800

    added test for BaseModel

---
+++ app/tests/Paxifi/Test/Support/Repository/BaseModelTest.php
@@ -0,0 +1,15 @@
+<?php namespace Paxifi\Test\Support\Repository;
+
+class BaseModelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidationPasses()
+    {
+        // TODO: Write test
+    }
+
+    public function testValidationFails()
+    {
+        // TODO: Write test
+    }
+}
+ 
