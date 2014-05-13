318a8c9efa4b23e7a65c10629bc01f39415d1ad3
Djamel Feddad <de.feddad@gmail.com>
2014-05-13 09:02:13 +0800

    added test for RemindersController

---
+++ app/tests/Paxifi/Test/Store/Controller/RemindersControllerTest.php
@@ -0,0 +1,26 @@
+<?php namespace Paxifi\Test\Store\Controller;
+
+class RemindersControllerTest extends \TestCase
+{
+
+    public function testSendReminderEmailToValidUser()
+    {
+        // TODO: Write test
+    }
+
+    public function testReturnErrorToInvalidUser()
+    {
+        // TODO: Write test
+    }
+
+    public function testShowResetPasswordForm()
+    {
+        // TODO: Write test
+    }
+
+    public function testResetPassword()
+    {
+        // TODO: Write test
+    }
+}
+ 
