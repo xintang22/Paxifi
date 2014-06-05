3adf81af380d3e9d843ea47401888aec4df35883
Djamel Feddad <djamel@mobilenowgroup.com>
2014-06-05 16:07:45 +0800

    updated tax field definition

--- app/database/migrations/2014_05_21_031612_create_products_table.php
+++ app/database/migrations/2014_05_21_031612_create_products_table.php
@@ -25,7 +25,7 @@ class CreateProductsTable extends Migration
             $table->decimal('unit_price');
             $table->decimal('average_cost');
             $table->unsignedInteger('inventory');
-            $table->decimal('tax');
+            $table->decimal('tax', 8, 5);
             $table->boolean('status')->default(1);
 
             $table->timestamps();
