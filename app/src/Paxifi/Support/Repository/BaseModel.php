226d93a1c6055754472a6d98aeba4ab205ba297c
Djamel Feddad <djamel@mobilenowgroup.com>
2014-06-17 15:36:38 +0800

    moved the data validation to dedicated validation class

--- app/src/Paxifi/Support/Repository/BaseModel.php
+++ app/src/Paxifi/Support/Repository/BaseModel.php
@@ -1,75 +1,12 @@
 <?php namespace Paxifi\Support\Repository;
 
 use Illuminate\Database\Eloquent\Model;
-use Paxifi\Support\Contracts\ValidatorInterface;
 
 /**
  * The Base Model using Eloquent ORM
  * @package Paxifi\Support\Repository
  */
-class BaseModel extends Model implements ValidatorInterface
+class BaseModel extends Model
 {
-    /**
-     * @var array
-     */
-    protected $rules = [];
 
-    /**
-     * @var array
-     */
-    protected static $errors;
-
-    /**
-     * @{@inheritdoc }
-     */
-    public function getRules()
-    {
-        return $this->rules;
-    }
-
-    /**
-     * @{@inheritdoc }
-     */
-    public function validate($data)
-    {
-        /** @var $validator \Illuminate\Validation\Validator */
-        $validator = \Validator::make($data, $this->rules);
-
-        // check for failure
-        if ($validator->fails()) {
-            // set errors and return false
-            self::$errors = $validator->errors()->all();
-            return false;
-        }
-
-        // validation pass
-        return true;
-    }
-
-    /**
-     * @{@inheritdoc }
-     */
-    public function getValidationErrors()
-    {
-        return self::$errors;
-    }
-
-    /**
-     * Save a new model and return the instance.
-     *
-     * @param  array $attributes
-     *
-     * @return \Illuminate\Database\Eloquent\Model|static
-     */
-    public static function create(array $attributes)
-    {
-        $model = new static($attributes);
-
-        if ($model->validate($attributes)) {
-
-            return parent::create($attributes);
-        }
-
-        return false;
-    }
 }
