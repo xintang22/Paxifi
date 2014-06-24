5d80769ab3726042348e3fe03d6a3b4096b8da90
Djamel Feddad <djamel@mobilenowgroup.com>
2014-06-24 16:39:37 +0800

    added total_tax field

--- app/database/migrations/2014_06_03_075720_create_orders_table.php
+++ app/database/migrations/2014_06_03_075720_create_orders_table.php
@@ -21,6 +21,7 @@ class CreateOrdersTable extends Migration {
             $table->unsignedInteger('total_items')->default(0);
             $table->decimal('total_costs')->default(0);
             $table->decimal('total_sales')->default(0);
+            $table->decimal('total_tax')->default(0);
             $table->decimal('profit')->default(0);
             $table->decimal('commission')->default(0);
 
