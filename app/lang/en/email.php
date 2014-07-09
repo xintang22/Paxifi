2484aac562f79ea4e978efec00940d0b0ae5894e
Sonny <334531994@qq.com>
2014-07-09 14:05:39 +0800

    added the translation for email

---
+++ app/lang/en/email.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    "sticker" => array(
+        "subject" => "Paxifi | Sticker",
+        "from" => "paxifi@paxifi.com",
+        "body" => "Dear :name, the sticker pdf is in attachment.",
+        "name" => "Paxifi Sticker"
+    ),
+    "invoice" => array(
+        "subject" => "Paxifi | Invoice",
+        "from" => "paxifi@paxifi.com",
+        "body" => "Dear :name, the invoice pdf is in attachment.",
+        "name" => "Paxifi Invoice"
+    ),
+);
