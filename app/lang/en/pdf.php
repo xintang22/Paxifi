35d177716ae64ea0200a4fc41e011404d9c1fc66
sonnyshen <334531994@qq.com>
2014-07-09 14:24:31 +0800

    added translation for invoice generator

---
+++ app/lang/en/pdf.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+    "content" => array(
+        // Header
+        "invoice" => "Invoice",
+
+        //Buyer Information
+        "billed" => "Billed to:",
+        "date" => "Date:",
+        "payment_amount" => "Payment Amount:",
+
+        // Seller Information
+        "seller" => "Seller:",
+        "seller_id" => "Seller ID:",
+        "email" => "Email:",
+
+        // Product
+        "name" => "Name",
+        "quantity" => "Quantity",
+        "price" => "Price",
+
+        "title" => "Title",
+        "description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "sub_total" => "Sub Total:",
+        "sales_tax" => "Sales Tax",
+        "total" => "Total"
+    ),
+);
