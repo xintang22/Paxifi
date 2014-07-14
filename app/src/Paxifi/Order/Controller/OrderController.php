aa396a34f97d900a35957d68f4d0ffdee83b3149
sonnyshen <334531994@qq.com>
2014-07-14 15:52:26 +0800

    added email invoice pdf api

--- app/src/Paxifi/Order/Controller/OrderController.php
+++ app/src/Paxifi/Order/Controller/OrderController.php
@@ -3,12 +3,12 @@
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Collection;
 use Paxifi\Order\Repository\EloquentOrderRepository;
+use Paxifi\Order\Repository\Factory\OrderInvoiceFactory;
 use Paxifi\Order\Transformer\OrderTransformer;
 use Paxifi\Support\Controller\ApiController;
 
 class OrderController extends ApiController
 {
-
     public function store()
     {
         try {
@@ -46,6 +46,73 @@ class OrderController extends ApiController
 
             return $this->errorWrongArgs($e->getMessage());
         }
+    }
+
+    /**
+     * Get order invoice email with a copy of invoice pdf file.
+     *
+     * @param $order
+     *
+     * @internal param $id
+     * @return mixed
+     */
+    public function email($order)
+    {
+        try {
+            \DB::beginTransaction();
+
+            $buyer_email = \Input::get('buyer_email');
+
+            if (empty($buyer_email))
+
+                return;
+
+            if ($order->status) {
+
+                $order->setBuyerEmail($buyer_email)
+                    ->save();
+
+                $invoiceFactory = new OrderInvoiceFactory($order, $this->getInvoiceContentTranslation());
+
+                $invoiceFactory->build();
+
+                // Config email options
+                $emailOptions = array(
+                    'context' => $this->translator->trans('email.invoice'),
+                    'to' => $buyer_email,
+                    'data' => $invoiceFactory->getInvoiceData(),
+                    'attach' => $invoiceFactory->getPdfFilePath(),
+                    'as' => 'invoice_' . $order->id . '.pdf',
+                    'mime' => 'application/pdf'
+                );
+
+                // Fire email invoice pdf event.
+                \Event::fire('email.invoice', array($emailOptions));
+
+                \DB::commit();
+
+                return $this->setStatusCode(200)->respond(
+                    [
+                        "success" => true,
+                    ]
+                );
+            }
+
+            return $this->setStatusCode(406)->respondWithError($this->translator->trans('responses.invoice.invoice_not_available', ['order_id' => $order->id]));
+
+        } catch (\Exception $e) {
+            return $this->errorWrongArgs($e->getMessage());
+        }
+    }
+
+    /**
+     * @internal param \Paxifi\Order\Repository\EloquentOrderRepository $order
+     *
+     * @return array
+     */
+    public function getInvoiceContentTranslation()
+    {
+        return $this->translator->trans('pdf.content');
     }
 
     /**
