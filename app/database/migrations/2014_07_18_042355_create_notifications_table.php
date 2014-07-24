c2881b86f0eb747a57214e229bec52d899fda2af
sonnyshen <334531994@qq.com>
2014-07-24 18:47:10 +0800

    added the notifications migrate table

---
+++ app/database/migrations/2014_07_18_042355_create_notifications_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('driver_id');
+            $table->integer('sales')->nullable()->default(0);
+            $table->string('ranking')->nullable()->default(NULL);
+            $table->integer('stock_reminder')->nullable()->default(0);
+            $table->integer('billing')->nullable()->default(0);
+            $table->integer('email')->nullable()->default(NULL);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign key.
+            $table->foreign('driver_id')->references('id')->on('drivers');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notifications');
+	}
+
+}
