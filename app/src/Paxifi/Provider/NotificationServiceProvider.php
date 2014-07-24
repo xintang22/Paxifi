6f40314d842ede13c23f57955ea0ae358e8bdcf5
sonnyshen <334531994@qq.com>
2014-07-25 03:31:58 +0800

    added the notification service provider

---
+++ app/src/Paxifi/Provider/NotificationServiceProvider.php
@@ -0,0 +1,67 @@
+<?php namespace Paxifi\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use Paxifi\Sticker\Exception\NotificationNotFoundException;
+
+/**
+ * Class NotificationServiceProvider
+ * @package Paxifi\Provider
+ */
+class NotificationServiceProvider extends ServiceProvider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerRoutes();
+
+        $this->registerEvents();
+
+        $this->registerNotificationRepository();
+    }
+
+    /**
+     * Register notification routes.
+     */
+    public function registerRoutes()
+    {
+        $this->app['router']->group(['before' => 'oauth'], function () {
+            $this->app['router']->post('notifications', 'Paxifi\Notification\Controller\NotificationController@store');
+        });
+    }
+
+    /**
+     * Register the events for notifications
+     */
+    public function registerEvents()
+    {
+        $this->app['events']->listen('paxifi.notifications.billing', 'Paxifi\Notification\Controller\NotificationController@billing');
+        $this->app['events']->listen('paxifi.notifications.sales', 'Paxifi\Notification\Controller\NotificationController@sales');
+        $this->app['events']->listen('paxifi.notifications.stock', 'Paxifi\Notification\Controller\NotificationController@stock');
+        $this->app['events']->listen('paxifi.notifications.ranking', 'Paxifi\Notification\Controller\NotificationController@ranking');
+        $this->app['events']->listen('paxifi.notifications.emails', 'Paxifi\Notification\Controller\NotificationController@emails');
+    }
+
+    /**
+     * Register the notification repository implementation.
+     *
+     * @return void
+     */
+    protected function registerNotificationRepository()
+    {
+        $this->app->bind('paxifi.repository.notifications', 'Paxifi\Notification\Repository\EloquentNotificationRepository', true);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('paxifi.repository.notifications');
+    }
+}
