7a810c09b1b33fadcd776397b9bca728de102e05
sonnyshen <334531994@qq.com>
2014-07-27 02:06:06 +0800

    updated the email queues accept the view data

--- app/src/Paxifi/Support/Queues/Queues.php
+++ app/src/Paxifi/Support/Queues/Queues.php
@@ -13,9 +13,14 @@ class Queues
      */
     public function email($job, $data)
     {
+        $view_data = [];
+
+        $view_data = !empty($data['data']) ? $data['data'] : [];
+
         // Process the send sticker pdf to user email event...
         try {
-            \Mail::send($data['template'], [], function ($message) use ($data) {
+
+            \Mail::send($data['template'], $view_data, function ($message) use ($data) {
 
                 $message->from($data['context']['from'], $data['context']['name'])->subject($data['context']['subject']);
 
@@ -24,15 +29,16 @@ class Queues
                 if (isset($data['attach'])) {
                     $message->attach($data['attach'], array('as' => $data['as'], 'mime' => $data['mime']));
                 }
-
             });
 
             if ($driver = Driver::findByEmail($data['to'])) {
+
                 \Event::fire('paxifi.notifications.emails', [$driver]);
             }
 
             $job->delete();
         } catch (\Exception $e) {
+            dd($e->getMessage());
             return \Response::json(["error" => true, "message" => ""], 500, []);
         }
     }
