9567d36a6bfa45387c68f00c9e42cd751879e3f8
sonnyshen <334531994@qq.com>
2014-07-29 13:09:39 +0800

    added the payment create validator

--- app/src/Paxifi/Payment/Repository/Validation/CreatePaymentValidator.php
+++ app/src/Paxifi/Payment/Repository/Validation/CreatePaymentValidator.php
@@ -4,7 +4,17 @@ use Paxifi\Support\Validation\Validator;
 
 class CreatePaymentValidator extends Validator
 {
+    /**
+     * @var array
+     */
     protected $rules = [
         'order_id' => 'unique:payments'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $messages = [
+        'unique' => 'The payment has already exists.'
     ];
 }
