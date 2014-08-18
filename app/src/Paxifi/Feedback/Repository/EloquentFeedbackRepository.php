466f7e7e30ac68808bd0f5ad8dd808bd9e1bdb64
sonnyshen <334531994@qq.com>
2014-08-18 11:58:59 +0800

    added the document for feedback relationships

--- app/src/Paxifi/Feedback/Repository/EloquentFeedbackRepository.php
+++ app/src/Paxifi/Feedback/Repository/EloquentFeedbackRepository.php
@@ -7,10 +7,20 @@ class EloquentFeedbackRepository extends BaseModel {
 
     protected $fillable = ['driver_id', 'payment_id', 'buyer_email', 'feedback', 'comment'];
 
+    /**
+     * Feedback - Driver many to one relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function driver() {
         return $this->belongsTo('Paxifi\Store\Repository\Driver\EloquentDriverRepository', 'driver_id', 'id');
     }
 
+    /**
+     * Feedback - Payment one to one relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function payment() {
         return $this->belongsTo('Paxifi\Payment\Repository\EloquentPaymentRepository', 'payment_id');
     }
