8c940959f30d2b097f688f73dd227cfe317e83aa
sonnyshen <334531994@qq.com>
2014-08-22 14:49:36 +0800

    updated routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,20 +1,10 @@
 <?php
 
-use Paxifi\Payment\Repository\EloquentPaymentRepository;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 
 App::missing(function($exception)
 {
-    if ($_SERVER['SCRIPT_NAME'] == '/index.php' && \Input::has('paypal_ipn')) {
-
-        $payment_id = \Input::get('custom');
-
-        $payment = EloquentPaymentRepository::find($payment_id);
-
-        return Response::json(json_decode($payment));
-    }
-
     return Response::json(array(
         "status" => 501,
         'error' => "not_implemented",
