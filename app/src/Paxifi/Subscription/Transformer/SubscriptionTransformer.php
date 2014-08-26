b2910a4c0b6e8e185b3e0ef2faa0d81bd779c13f
sonnyshen <334531994@qq.com>
2014-08-27 00:34:51 +0800

    updated subscription transformer

--- app/src/Paxifi/Subscription/Transformer/SubscriptionTransformer.php
+++ app/src/Paxifi/Subscription/Transformer/SubscriptionTransformer.php
@@ -4,10 +4,26 @@ use League\Fractal\TransformerAbstract;
 
 class SubscriptionTransformer extends TransformerAbstract
 {
-    public function transformer($subscription)
+    public function transform($subscription)
     {
         return [
             'id' => $subscription->id,
+            'driver_id' => $subscription->driver_id,
+            'trial_start' => $subscription->trial_start,
+            'trial_end' => $subscription->trial_end,
+            'start' => $subscription->start,
+            'canceled_at' => $subscription->canceled_at,
+            'ended_at' => $subscription->ended_at,
+            'current_period_start' => $subscription->current_period_start,
+            'current_period_end' => $subscription->current_period_end,
+            'cancel_at_period_end' => $subscription->cancel_at_period_end,
+            'status' => $subscription->status,
+
         ];
+    }
+
+    public function transformSubscriptionPlan($id)
+    {
+
     }
 } 
