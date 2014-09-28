8f22dfd364523a7a13722f88f24ad7e9f4ff5340
Sonny Shen <334531994@qq.com>
2014-09-28 23:44:40 +0800

    updated commission transformer.

--- app/src/Paxifi/Commission/Transformer/CommissionTransformer.php
+++ app/src/Paxifi/Commission/Transformer/CommissionTransformer.php
@@ -5,14 +5,20 @@ namespace Paxifi\Commission\Transformer;
 use League\Fractal\TransformerAbstract;
 use Paxifi\Commission\Repository\EloquentCommissionRepository;
 
-class CommissionTransformer extends TransformerAbstract {
+class CommissionTransformer extends TransformerAbstract
+{
 
-    public function transform(EloquentCommissionRepository $commission) {
+    public function transform(EloquentCommissionRepository $commission)
+    {
         return [
             'id' => $commission->id,
             'driver_id' => $commission->driver_id,
             'status' => $commission->status,
-            'total_commission' => $commission->total_commission
+            'commissions' => $commission->commissions,
+            'capture_id' => $commission->capture_id,
+            'capture_status' => $commission->capture_status,
+            'capture_created_at' => $commission->capture_created_at,
+            'capture_updated_at' => $commission->capture_updated_at,
         ];
-}
+    }
 } 
