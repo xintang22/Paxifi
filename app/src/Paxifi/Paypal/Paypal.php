58a833a8b267e0d48063ddb448505d2b9b436d33
Sonny Shen <334531994@qq.com>
2014-11-06 17:01:02 +0800

    updated paypal class

--- app/src/Paxifi/Paypal/Paypal.php
+++ app/src/Paxifi/Paypal/Paypal.php
@@ -28,6 +28,66 @@ class Paypal
         $this->request = $request;
     }
 
+    /**
+     * Get paypal api link
+     *
+     * @param array $links
+     * @param       $rel
+     *
+     * @return string
+     */
+    private function getPaypalLink(array $links = array(), $rel)
+    {
+        $matchedUrl = "";
+
+        foreach ($links as $index => $link) {
+            if ($link->rel == $rel) {
+                $matchedUrl = $link->href;
+            }
+        }
+
+        return $matchedUrl;
+    }
+
+    /**
+     * Get the authorize paypal transaction object.
+     *
+     * @param float  $amount
+     * @param string $currency
+     *
+     * @param string $description
+     *
+     * @return array
+     */
+    public function getFuturePaymentTransaction($amount = 0.1, $currency = 'USD', $description = 'Paxifi PayPal Transaction') {
+        $transaction = [
+            'intent' => 'authorize',
+            'payer' => [
+                'payment_method' => 'paypal'
+            ],
+            'transactions' => [
+                [
+                    "amount" => [
+                        "currency" => $currency,
+                        "total" => $amount
+                    ],
+                    "description" => $description
+                ]
+            ]
+        ];
+
+        return $transaction;
+    }
+
+    /**
+     * Curl PayPal request.
+     *
+     * @param        $url
+     * @param string $method
+     * @param null   $postvals
+     *
+     * @return array
+     */
     private function curl($url, $method = 'GET', $postvals = null)
     {
         $ch = curl_init($url);
@@ -59,132 +119,6 @@ class Paypal
     }
 
     /**
-     * Verify given authorization code.
-     *
-     * @param string $code
-     * @param bool   $attachToRequest
-     *
-     * @throws \InvalidArgumentException
-     * @return mixed
-     */
-    public function verifyAuthorizationCode($code, $attachToRequest = false)
-    {
-        $oauth2Url = $this->paypalUrl . 'oauth2/token';
-
-         return $this->requestWithCurl($code, $attachToRequest, $oauth2Url);
-//
-//        return $this->requestWithGuzzle($code, $attachToRequest, $oauth2Url);
-
-    }
-
-    /**
-     * Get the user access token using his stored refresh token
-     *
-     * @param EloquentDriverRepository $driver
-     *
-     * @throws \Exception
-     * @return string
-     */
-    public function getUserAccessToken(EloquentDriverRepository $driver)
-    {
-        try {
-            $oauth2Url = $this->paypalUrl . 'oauth2/token';
-
-            $postvals = "grant_type=refresh_token&refresh_token=$driver->paypal_refresh_token";
-
-            $response = self::curl($oauth2Url, "POST", $postvals);
-
-            if ($response['status'] == 200) {
-                return $response['body']->access_token;
-            }
-
-            throw new \Exception('Refresh access token failed.');
-        } catch (\Exception $e) {
-            throw new \Exception($e->getMessage());
-        }
-    }
-
-    /**
-     * Retrieve user profile attributes by given access token.
-     *
-     * @param $accessToken
-     *
-     * @return mixed
-     */
-    public function getUserInfoByAccessToken($accessToken)
-    {
-
-        // Create a fake payment to check the user paypal account
-        // and store his paypal email (metchant email)
-        $transactions = [
-            'intent' => 'authorize',
-            'payer' => [
-                'payment_method' => 'paypal'
-            ],
-            'transactions' => [
-                [
-                    "amount" => [
-                        "currency" => 'USD',
-                        "total" => 0.01
-                    ],
-                    "description" => "Paxifi: check validity of PayPal account"
-                ]
-            ]
-        ];
-
-        $payment = $this->createPayment($accessToken, $transactions);
-
-        // Capture the payment
-        $capturedPayment = $this->capturePayment($accessToken, $payment);
-
-        // Refund the payment
-        if ($this->refundPayment($accessToken, $capturedPayment)) {
-            return $payment->payer->payer_info;
-        } else {
-            return fasle;
-        }
-    }
-
-    /**
-     * create subscription payment via paypal.
-     *
-     * @param EloquentPlanRepository   $plan
-     * @param EloquentDriverRepository $driver
-     *
-     * @return bool|mixed
-     */
-    public function subscriptionPayment(EloquentPlanRepository $plan, EloquentDriverRepository $driver)
-    {
-        $transactions = [
-            'intent' => 'authorize',
-            'payer' => [
-                'payment_method' => 'paypal'
-            ],
-            'transactions' => [
-                [
-                    "amount" => [
-                        "currency" => $plan->currency,
-                        "total" => $plan->amount
-                    ],
-                    "description" => "Paxifi: Subscription payment."
-                ]
-            ]
-        ];
-
-        $payment = $this->createPayment(null, $transactions, $driver);
-
-        // Capture the payment
-        if ($capturedPayment = $this->capturePayment(null, $payment, $driver)) {
-            // Todo:: record capture payment success.
-
-            return $capturedPayment;
-        } else {
-            // Todo:: record Capture payment failed.
-            return false;
-        }
-    }
-
-    /**
      * Create a payment.
      *
      * @param       $accessToken
@@ -208,19 +142,16 @@ class Paypal
                 'json' => $transactions
             ]);
 
-            if ($res->getStatusCode() == 201) {
-//                PaypalLog::info(['Create' => $res->json()]);
+            if ($res->getStatusCode() == 201 && $res->json(['object' => true])->state == 'approved') {
                 // Todo:: record create authorized future payment success.
                 return $res->json(['object' => true]);
             }
 
-//            PaypalLog::error(['Create future payment failed']);
-            throw new \Exception('Create future payment failed');
+            // Todo:: record create authorized future payment failed.
         } catch (\Exception $e) {
 
             // Todo:: record create authorized future payment failed.
-//            PaypalLog::error(['Create future payment failed']);
-            throw new \Exception($e->getMessage());
+
         }
     }
 
@@ -256,20 +187,23 @@ class Paypal
             if ($capture->getStatusCode() == 200) {
 
                 if (($capture->json(['object' => true])->state == 'pending' &&
-                    $payment->transactions[0]->related_resources[0]->authorization->state == 'pending' &&
-                    $payment->transactions[0]->related_resources[0]->authorization->reason_code == 'PAYMENT_REVIEW') ||
+                        $payment->transactions[0]->related_resources[0]->authorization->state == 'pending' &&
+                        $payment->transactions[0]->related_resources[0]->authorization->reason_code == 'PAYMENT_REVIEW') ||
                     $capture->json(['object' => true])->state == "completed"
                 ) {
+                    // Todo:: record capture payment success.
+
                     return $capture->json(['object' => true]);
                 }
+
+                // Todo:: record capture payment failed.
             }
 
-//            PaypalLog::error(['Capture future payment failed']);
-            throw new \Exception('Capture future payment failed');
+            // Todo:: record capture payment error.
         } catch (\Exception $e) {
 
-//            PaypalLog::error(['Capture future payment failed']);
-            throw new \Exception($e->getMessage());
+            // Todo:: record capture payment failed.
+
         }
     }
 
@@ -293,38 +227,142 @@ class Paypal
             ]);
 
             if ($refund->getStatusCode() == 201) {
-//                PaypalLog::info(['Refund' => $refund->json()]);
+                // Todo:: record refund payment success.
                 return true;
             }
+            // Todo:: record refund payment failed.
 
-//            PaypalLog::error(['Refund failed']);
             return false;
         } catch (\Exception $e) {
-//            PaypalLog::error(['Refund failed']);
-            throw new \Exception($e->getMessage());
+
+            // Todo:: record refund payment failed.
+
         }
 
     }
 
     /**
-     * Get paypal api link
+     * Verify given authorization code.
      *
-     * @param array $links
-     * @param       $rel
+     * @param string $code
+     * @param bool   $attachToRequest
      *
+     * @throws \InvalidArgumentException
+     * @return mixed
+     */
+    public function verifyAuthorizationCode($code, $attachToRequest = false)
+    {
+        $oauth2Url = $this->paypalUrl . 'oauth2/token';
+
+         return $this->requestWithCurl($code, $attachToRequest, $oauth2Url);
+
+//        return $this->requestWithGuzzle($code, $attachToRequest, $oauth2Url);
+
+    }
+
+    /**
+     * Get the user access token using his stored refresh token
+     *
+     * @param EloquentDriverRepository $driver
+     *
+     * @throws \Exception
      * @return string
      */
-    private function getPaypalLink(array $links = array(), $rel)
+    public function getUserAccessToken(EloquentDriverRepository $driver)
     {
-        $matchedUrl = "";
+        try {
+            $oauth2Url = $this->paypalUrl . 'oauth2/token';
 
-        foreach ($links as $index => $link) {
-            if ($link->rel == $rel) {
-                $matchedUrl = $link->href;
+            $postvals = "grant_type=refresh_token&refresh_token=$driver->paypal_refresh_token";
+
+            $response = self::curl($oauth2Url, "POST", $postvals);
+
+            if ($response['status'] == 200) {
+                return $response['body']->access_token;
+            }
+
+            // Todo:: record get user access token failed.
+        } catch (\Exception $e) {
+            // Todo:: record get user access token failed.
+        }
+    }
+
+    /**
+     * Retrieve user profile attributes by given access token.
+     *
+     * @param $accessToken
+     *
+     * @return mixed
+     */
+    public function getUserInfoByAccessToken($accessToken)
+    {
+
+        // Create a fake payment to check the user PayPal account
+        // and store his PayPal email (merchant email)
+        $transaction = $this->getFuturePaymentTransaction(0.1, 'USD', 'Paxifi: check validity of PayPal account');
+
+        $payment = $this->createPayment($accessToken, $transaction);
+
+        // Capture the payment
+        $capturedPayment = $this->capturePayment($accessToken, $payment);
+
+        // Refund the payment
+        if ($this->refundPayment($accessToken, $capturedPayment)) {
+            return $payment->payer->payer_info;
+        } else {
+            return fasle;
+        }
+    }
+
+    /**
+     * create subscription payment via paypal.
+     *
+     * @param EloquentPlanRepository   $plan
+     * @param EloquentDriverRepository $driver
+     *
+     * @return bool|mixed
+     */
+    public function subscriptionPayment(EloquentPlanRepository $plan, EloquentDriverRepository $driver)
+    {
+
+        $transaction = $this->getFuturePaymentTransaction($plan->amount, $plan->currency, "Paxifi: Subscription payment.");
+
+        if ($payment = $this->createPayment(null, $transaction, $driver)) {
+            // Todo:: record create subscription payment success.
+
+            // Capture the payment
+            if ($capturedPayment = $this->capturePayment(null, $payment, $driver)) {
+                // Todo:: record capture subscription payment success.
+
+                return $capturedPayment;
+            } else {
+                // Todo:: record capture subscription payment failed.
+                return false;
             }
         }
 
-        return $matchedUrl;
+    }
+
+    /**
+     * @param                          $commission
+     * @param EloquentDriverRepository $driver
+     *
+     * @internal param $sales
+     * @return bool|mixed
+     */
+    public function commissionPayment($commission, EloquentDriverRepository $driver) {
+        $transaction = $this->getFuturePaymentTransaction($commission, $driver->currency, "Paxifi: Commission payment.");
+
+        $payment = $this->createPayment(null, $transaction, $driver);
+
+        // Capture the payment
+        if ($capturedPayment = $this->capturePayment(null, $payment, $driver)) {
+            // Todo:: record capture commission payment success.
+            return $capturedPayment;
+        } else {
+            // Todo:: record capture comission payment failed.
+            return false;
+        }
     }
 
     /**
