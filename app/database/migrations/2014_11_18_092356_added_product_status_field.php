0acedd64cf96d8e586886d86150db4cb33a76645
Sonny Shen <334531994@qq.com>
2014-11-18 17:32:27 +0800

    added status field to products

---
+++ app/database/migrations/2014_11_18_092356_added_product_status_field.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddedProductStatusField extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        if (Schema::hasColumn('products', 'status'))
+        {
+            Schema::table('products', function($table)
+            {
+                $table->dropColumn('status');
+            });
+        }
+
+        Schema::table('products', function($table)
+        {
+            $table->boolean('status')->default(1);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('products', function($table)
+        {
+            $table->dropColumn('status');
+        });
+	}
+
+}
