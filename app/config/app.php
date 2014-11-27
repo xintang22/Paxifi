e961f3620705dc899a69a222f42de0669c59a556
Sonny Shen <334531994@qq.com>
2014-11-27 10:50:37 +0800

    added secure url app config

--- app/config/app.php
+++ app/config/app.php
@@ -28,6 +28,19 @@ return array(
 
 	'url' => 'http://api.paxifi.com',
 
+    /*
+	|--------------------------------------------------------------------------
+	| Application URL
+	|--------------------------------------------------------------------------
+	|
+	| This URL is used by the console to properly generate URLs when using
+	| the Artisan command line tool. You should set this to the root of
+	| your application so that it is used when running Artisan tasks.
+	|
+	*/
+
+    'secure' => $_ENV['SECURE'],
+
 	/*
 	|--------------------------------------------------------------------------
 	| Application Timezone
