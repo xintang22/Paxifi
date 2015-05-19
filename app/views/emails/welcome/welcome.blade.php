e5118d7133458b2eb1b2d02f13c01951649ccc76
Djamel Feddad <djamel@mobilenowgroup.com>
2015-05-19 12:47:57 +0800

    handle new registered user event

---
+++ app/views/emails/welcome/welcome.blade.php
@@ -0,0 +1,80 @@
+<!-- Inliner Build Version 4380b7741bb759d6cb997545f3add21ad48f010b -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width" />
+</head>
+<body style="width: 100% !important; min-width: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; margin: 0; padding: 0;"><style type="text/css">
+    @media only screen and (max-width: 600px) {
+        table.body {
+            line-height: 20px !important;
+        }
+        .container {
+            width: 95% !important; padding-left: 0 !important; padding-right: 0 !important;
+        }
+        table.header td.first {
+            padding: 10px 1px !important;
+        }
+        table.header td.last {
+            font-size: 14px !important; padding-right: 0 !important;
+        }
+        p.email {
+            font-size: 18px !important;
+        }
+        p.steps {
+            font-size: 18px !important;
+        }
+        hr {
+            margin-left: 0 !important; margin-right: 0 !important;
+        }
+        table.footer img.middle {
+            margin-left: 15px !important; margin-right: 15px !important;
+        }
+        div.welcome {
+            width: 95% !important; margin-left: 0 !important; font-size: 13px !important; line-height: 1 !important;
+        }
+        div.welcome h1 {
+            margin-top: 12px !important; margin-bottom: 8px !important; font-size: 20px !important;
+        }
+        p.steps span {
+            width: 15px !important;
+        }
+        p.steps b {
+            margin-left: 10px !important; margin-right: 10px !important;
+        }
+    }
+</style>
+<table class="body" style="border-spacing: 0; border-collapse: collapse; height: 100%; width: 100%; font-family: Helvetica, arial, sans-serif; line-height: 30px;"><tr><td style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important;">
+            <table class="header container" style="border-spacing: 0; border-collapse: collapse; width: 700px; padding-left: 18px; padding-right: 18px; background-size: contain; background: url('}'); margin: 0 auto;" background="url(&quot;}&quot;)"><tr><td class="first" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; background-size: contain; background: url('}') no-repeat; padding: 28px 0 28px 18px;">
+                        <img src="{{ cloudfront_asset('emails/welcome/paxifi_logo.jpg') }}" alt="Paxifi logo" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 100%; float: left; clear: both; display: block;" align="left" /></td>
+                    <td class="last" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; text-align: right; padding-right: 18px; font-size: 24px; color: #999999; background-size: contain; background: url('}') no-repeat;" align="right">
+                        <span>Stores on wheels</span>
+                    </td>
+                </tr></table><table class="main container" style="border-spacing: 0; border-collapse: collapse; width: 700px; padding-left: 18px; padding-right: 18px; background-size: contain; background: url('}'); margin: 0 auto;" background="url(&quot;}&quot;)"><tr><td style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important;">
+                        <table style="border-spacing: 0; border-collapse: collapse;"><tr><td class="container" style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; width: 700px; padding-left: 18px; padding-right: 18px; margin: 0 auto;">
+                                    <div class="welcome" style="position: absolute; width: 700px; margin-left: -18px; color: #FFFFFF; font-size: 24px;">
+                                        <h1 class="center" style="text-align: center; font-size: 36px; margin-top: 30px; margin-bottom: 15px;" align="center">Welcome to Paxifi</h1>
+                                        <p class="center" style="text-align: center; margin: 0;" align="center">Thanks for signing up!</p>
+                                        <p class="center" style="text-align: center; margin: 0;" align="center">Get your Paxifi account ready and join our beta</p>
+                                        <p class="center" style="text-align: center; margin: 0;" align="center">phase to get a chance to win $1000</p>
+                                    </div>
+                                    <img src="{{ cloudfront_asset('emails/welcome/topbar_bg.jpg') }}" alt="Top bar" class="welcome" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: 100%; max-width: 100%; float: left; clear: both; display: block; margin-bottom: 54px;" align="left" /><p class="steps center" style="text-align: center; font-size: 30px; color: #F7941E; margin: 0;" align="center">
+                                        <span style="display: inline-block; width: 28px; height: 20px; background: white url('}') no-repeat center bottom;"></span><b style="margin-left: 20px; margin-right: 20px;">3 STEPS TO SET UP YOUR</b><span style="display: inline-block; width: 28px; height: 20px; background: white url('}') no-repeat center bottom;"></span>
+                                    </p>
+                                    <p class="steps center" style="text-align: center; font-size: 30px; color: #F7941E; margin: 0;" align="center">
+                                        <b style="margin-left: 20px; margin-right: 20px;">STORE ON WHEELS</b>
+                                    </p>
+                                    <img src="{{ cloudfront_asset('emails/welcome/3steps.jpg') }}" class="steps" alt="4 steps" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: 100%; max-width: 100%; float: left; clear: both; display: block; margin-top: 54px;" align="left" /></td>
+                            </tr></table><hr class="top" style="height: 2px; background: url('}'); margin: 74px 18px 40px; border: none;" /><table class="row" style="border-spacing: 0; border-collapse: collapse; width: 100%; position: relative; padding: 0px;"><tr><td style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important;">
+                                    <p class="contact-us center" style="text-align: center; font-size: 24px; color: #F6911D; line-height: 25px; margin: 0 0 20px;" align="center"><b>Contact Us</b></p>
+                                    <p class="center email" style="text-align: center; font-size: 24px; color: #494949; margin: 0;" align="center">Stay in touch with our Paxifi team if you have any question or need any support to be Paxify ready: <a href="mailto:admin@paxifi.com" style="color: #F7941E; text-decoration: none;">admin@paxifi.com</a><br /> Visit this website <a href="http://home.paxifi.com" style="color: #F7941E; text-decoration: none;">http://home.paxifi.com</a>
+                                    </p>
+                                </td>
+                            </tr></table><hr class="bottom" style="height: 2px; background: url('}'); margin: 35px 18px 0; border: none;" /><table class="row footer" style="border-spacing: 0; border-collapse: collapse; background-size: contain; width: 100%; position: relative; background: url('}') no-repeat bottom; padding: 0px;" background="url(&quot;}&quot;)"><tr><td style="word-break: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; padding-top: 18px; padding-bottom: 12px;">
+                                    <div class="center" style="text-align: center;" align="center"><a href="https://twitter.com/paxifi"><img src="{{ cloudfront_asset('emails/welcome/twitter.jpg') }}" alt="Twitter" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 100%; float: none; clear: both; display: inline-block; border: none;" align="none" /></a><a href="https://www.facebook.com/paxifiapp"><img src="{{ cloudfront_asset('emails/welcome/facebook.jpg') }}" alt="Facebook" class="middle" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 100%; float: none; clear: both; display: inline-block; margin-left: 38px; margin-right: 38px; border: none;" align="none" /></a><a href="https://www.youtube.com/watch?v=wiy8zJnEO2A"><img src="{{ cloudfront_asset('emails/welcome/youtube.jpg') }}" alt="Youtube" style="outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; width: auto; max-width: 100%; float: none; clear: both; display: inline-block; border: none;" align="none" /></a></div>
+                                </td>
+                            </tr></table></td>
+                </tr></table><!-- End of the container for main content --></td>
+    </tr></table></body>
+</html>
